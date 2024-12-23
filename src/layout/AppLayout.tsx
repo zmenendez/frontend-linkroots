c87fba7945299d474961e627cda30079f88bfe9b
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-12-22 20:14:54 -0600

    Add AppLayout

---
+++ src/layout/AppLayout.tsx
@@ -0,0 +1,50 @@
+import { Link, Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
+
+export default function AppLayout() {
+
+
+    return (
+        <>
+            <header className="bg-slate-800 py-5">
+                <div className="mx-auto max-w-5xl flex flex-col md:flex-row items-center md:justify-between">
+                    <div className="w-full p-5 lg:p-0 md:w-1/3">
+                        <img src="/logo.svg" className="w-full block" />
+                    </div>
+                    <div className="md:w-1/3 md:flex md:justify-end">
+                        <button
+                            className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
+                            onClick={() => {}}
+                        >
+                            Cerrar Sesión
+                        </button>
+                    </div>
+                </div>
+            </header>
+            <div className="bg-gray-100  min-h-screen py-10">
+                <main className="mx-auto max-w-5xl p-10 md:p-0">
+
+                    
+                    <div className="flex justify-end">
+                        <Link 
+                            className="font-bold text-right text-slate-800 text-2xl"
+                            to={''}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >Visitar Mi Perfil</Link>
+                    </div>
+
+                    <div className="flex flex-col md:flex-row gap-10 mt-10">
+                        <div className="flex-1 ">
+                            <Outlet />
+                        </div>
+                        <div className="w-full md:w-96 bg-slate-800 px-5 py-10 space-y-6">
+
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <Toaster position="top-right" />
+        </>
+    )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
 import AuthLayout from "./layout/AuthLayout";
+import AppLayout from "./layout/AppLayout";
 
 export default function Router() {
     return (
@@ -10,6 +11,10 @@ export default function Router() {
                 <Route element={<AuthLayout />} >
                     <Route path='/auth/login' element={<LoginView />} />
                     <Route path='/auth/register' element={<RegisterView />} />
+                </Route>
+
+                <Route path= '/admin' element={<AppLayout />} >
+                    
                 </Route>
             </Routes>
         </BrowserRouter>
